4a64dab20b1be67a0c06bada14b17eac275af353
Mikhaylyuk Sergey <s.mikhaylyuk@hacken.io>
2024-02-27 12:12:55 +0000

    email done

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,5 +1,6 @@
-import { changeAvatar, signupService, loginService } from "../services/usersServices.js";
+import { changeAvatar, signupService, loginService, resendVerify, verification } from "../services/usersServices.js";
 import { User } from "../models/userModel.js";
+import HttpError from "../helpers/HttpError.js";
 
 export const signup = async (req, res, next) => {
 	try {
@@ -65,5 +66,43 @@ export const updateAvatar = async (req, res, next) => {
 		res.json({ avatarURL });
 	} catch (error) {
 		next(error);
+	}
+};
+
+export const verifyEmail = async (req, res, next) => {
+	const { verificationToken } = req.params;
+	try {
+		await verification(verificationToken);
+
+		res.json({
+			message: "Verification successful",
+		});
+	} catch (err) {
+		if (err.status === 404) {
+			return res.status(409).json({
+				message: err.message,
+			});
+		} else {
+			next(HttpError(err.status));
+		}
+	}
+};
+
+export const resendVerifyEmail = async (req, res, next) => {
+	const { email } = req.body;
+	try {
+		await resendVerify(email);
+
+		res.json({
+			message: "Verification email sent",
+		});
+	} catch (err) {
+		if (err.status === 404 || err.status === 400) {
+			return res.status(err.status).json({
+				message: err.message,
+			});
+		} else {
+			next(HttpError(err.status));
+		}
 	}
 };
